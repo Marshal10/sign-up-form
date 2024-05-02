a24f5ccbc7d4f9887e4f6e5155c6dfa74ff3659a
Marshal10 <marshalmaleane123@gmail.com>
2024-05-02 16:38:39 +0530

    Form btn not functional

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container=document.querySelector('.container')
 const signBtn=document.querySelector('.green-bg button')
+const rightBtn=document.querySelector('.signup-form button')
 
 signBtn.addEventListener('click',()=>{
     container.classList.toggle('change')
+})
+
+rightBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    alert("This form is not functional 🙂")
 })
